b313d24e3193181fb41b8ab09878f2518d87c53d
zgigorigor <49338720+zgigorigor@users.noreply.github.com>
2019-07-06 12:58:50 +0200

    zadatak2, pocetak

--- main_zadatak2.cpp
+++ main_zadatak2.cpp
@@ -1,3 +1,11 @@
+/*
+2. Netko je mijenjao Mirekovu random funkciju te ona više ne vraæa pseudosluèajne brojeve. 
+Sada je oèito da funkcija vraæa èešæe brojeve èija je meðusobna razlika toèno K. 
+Kako je Mirek znatiželjan po prirodi zanima ga koliko parova brojeva (od N razlièitih) ima razliku K.
+
+Pomoæ:  Koristite standardne funkcije za pretraživanje i sortiranje
+*/
+
 #include <iostream>
 
 
