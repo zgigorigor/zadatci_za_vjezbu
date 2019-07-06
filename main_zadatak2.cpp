aa49569f8dec15dc40a0073deb242f86a82f6a23
zgigorigor <49338720+zgigorigor@users.noreply.github.com>
2019-07-06 13:34:44 +0200

    zadatak2

--- main_zadatak2.cpp
+++ main_zadatak2.cpp
@@ -7,10 +7,49 @@ Pomoæ:  Koristite standardne funkcije za pretraživanje i sortiranje
 */
 
 #include <iostream>
+#include <time.h>
+#include <algorithm>
 
+#define MAX_SIZE = 100;
 
 int main()
 {
+	int generatedNumber = 0;
+	int	N, K;
+
+	std::cout << "What difference of your pairs do you want? " << std::endl;
+	std::cin >> K;
+
+
+	std::cout << "How many pair of numbers you want to check? " << std::endl;
+	std::cin >> N;
+
+	if (N < 100)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			srand(time(NULL));
+			generatedNumber = rand() % 100;
+			//std::cout << generatedNumber << "\n";
+			if (generatedNumber = K)
+			{
+
+			}
+		}
+
+	}
+	else
+	{
+		std::cout << "Chosen nmumber is out of range." << "\n";
+	}
+	
+		
+	/*
+	srand((unsigned)time(0)); 
+    int i;
+    i = (rand()%6)+1; 
+    cout << i << "\n"; 
+	*/
 
 	std::system("pause");
 }
